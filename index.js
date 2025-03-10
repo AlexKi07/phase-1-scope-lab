e0f2107b490951c0bd03e7589bfa892d33f28a0f
AlexKi07 <kimanialexk07@gmail.com>
2025-03-10 23:05:38 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = "bob"; // Global variable
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase(); // Modifies the global variable
+}
+
+
+function setBestCustomer() {
+    bestCustomer = "not bob"; // Assign value in global scope
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "steve"
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "mary"
+}
